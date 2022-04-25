25451fc6b314bc9c6aede98c5900b3ff993ed18b
ppmyor <yorocobe@gmail.com>
2022-04-26 00:08:19 +0900

    Baekjoon - 입출력 2438번 코드 리팩토링

--- Baekjoon/2438.js
+++ Baekjoon/2438.js
@@ -1,8 +1,8 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 const testCase = parseInt(input[0]);
-let answer = "";
+
+const star = "*";
 
 for (let i = 1; i <= testCase; i++) {
-    answer += "*";
-    console.log(answer);
+    console.log(star.repeat(i));
 }
