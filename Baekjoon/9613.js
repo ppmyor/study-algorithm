4cb59e889d6db479e6a35558904e48f884effb22
ppmyor <yorocobe@gmail.com>
2022-05-12 08:23:48 +0900

    Baekjoon - 기초 수학 9613번 코드 리팩토링

--- Baekjoon/9613.js
+++ Baekjoon/9613.js
@@ -6,10 +6,9 @@ const gcd = (a, b) => (a % b ? gcd(b, a % b) : b);
 
 for (let i = 1; i <= testCase; i++) {
   const [, ...numbers] = input[i].split(" ").map(Number);
-  const numTestCase = numbers[0];
   let result = 0;
-  for (let j = 1; j <= numTestCase; j++) {
-    for (let k = j + 1; k <= numTestCase; k++) {
+  for (let j = 0; j < numbers.length; j++) {
+    for (let k = j + 1; k < numbers.length; k++) {
       const [num1, num2] = [Math.max(numbers[j], numbers[k]), Math.min(numbers[j], numbers[k])];
       result += gcd(num1, num2);
     }
