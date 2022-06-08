b40805ca90d018d70de48eb52d84e788e5286878
ppmyor <yorocobe@gmail.com>
2022-06-08 16:32:00 +0900

    Baekjoon - 기초 수학 1676번 - 팩토리얼 0의 개수 리팩토링

--- Baekjoon/1676.js
+++ Baekjoon/1676.js
@@ -1,20 +1,10 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
-const num = parseInt(input[0]);
-let result = 1n;
+let num = Number(input[0]);
 let answer = 0;
 
-for (let i = 1n; i <= num; i++) {
-  result = result * i;
-}
-
-result = String(result).split("").reverse();
-
-for (let i in result) {
-  if (result[i] === "0") {
-    answer++;
-  } else {
-    break;
-  }
+while (num >= 5) {
+  answer += Math.floor(num / 5);
+  num /= 5;
 }
 
 console.log(answer);
