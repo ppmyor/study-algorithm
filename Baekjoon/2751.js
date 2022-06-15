1ff1ef2e823fd4ca47a1bc18817ea8550c16ce38
ppmyor <yorocobe@gmail.com>
2022-06-15 12:24:22 +0900

    Baekjoon - 정렬 2751번 -  수 정렬하기 2 mergeSort 리팩토링

--- Baekjoon/2751.js
+++ Baekjoon/2751.js
@@ -6,5 +6,32 @@ const input = require("fs")
   .map((v) => Number(v));
 
 const [testCase, ...num] = input;
-const result = num.sort((a, b) => a - b);
-console.log(result.join("\n"));
+
+const mergeSort = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+};
+
+console.log(mergeSort(num).join("\n"));
